5c475f7e188a927e1d487fd10a3748018b2353a4
c.mj2000@yahoo.com <c.mj2000@yahoo.com>
2018-05-16 23:19:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function getFirstSelector(selector){
+  const x = document.querySelector(selector)
+  return x
+}
+
+function nestedTarget(){
+  const x = document.querySelector("#nested .target")
+  return x
+}
+
+function increaseRankBy(n){
+   var x = document.querySelectorAll(".ranked-list")
+  
+  for(var i = 0; i < x.length; i++){
+    var y = x[i].querySelectorAll("li")
+    for(var j = 0; j<y.length; j++){
+      y[j].innerHTML = parseInt(y[j].innerHTML) + n
+      
+    }
+  }
+}
+
+function deepestChild(){
+  var x = document.querySelector('#grand-node div div div div')
+  
+  //var x = document.querySelector("div#grand-node")
+ // console.log(x.length)
+ return x
+}
