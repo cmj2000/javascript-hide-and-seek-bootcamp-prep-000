8071584be1507b14b28abae1f92614794be99fd8
c.mj2000@yahoo.com <c.mj2000@yahoo.com>
2018-05-16 22:58:55 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,30 @@
+function getFirstSelector(selector){
+  const x = document.querySelector(selector)
+  return x
+}
+
+function nestedTarget(){
+  const x = document.querySelector("#nested .target")
+  return x
+}
+
+function increaseRankBy(n){
+   var x = document.querySelectorAll(".ranked-list")
+  
+  for(var i = 0; i < x.length; i++){
+    var y = x[i].querySelectorAll("li")
+    for(var j = 0; j<y.length; j++){
+      y[j].innerHTML = parseInt(y[j].innerHTML) + n
+      
+    }
+  }
+}
+
+function deepestChild(){
+  var x = document.querySelector("div#grand-node")
+  console.log(x)
+  for(var i = 0; i < x.length; i++){
+    
+    
+  }
+}
