754c777f30fb1e4d3041ef028dd68b45f3baf08e
c.mj2000@yahoo.com <c.mj2000@yahoo.com>
2018-05-16 23:10:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function increaseRankBy(n){
 function deepestChild(){
   //var x = document.querySelector('#grand-node div div div div')
   
-  var x = document.querySelector("#grand-node").lastChild
+  var x = document.querySelector("div#grand-node").lastChild
   console.log(x)
  return x
 }
