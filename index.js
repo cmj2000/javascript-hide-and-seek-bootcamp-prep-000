0cc3a9d0821f3417655cc1d296025c42585b49bb
c.mj2000@yahoo.com <c.mj2000@yahoo.com>
2018-05-16 23:03:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,10 +21,7 @@ function increaseRankBy(n){
 }
 
 function deepestChild(){
-  var x = document.getElementById('grand-node')
+  var x = document.getElementById('grand-node');
   console.log(x)
-  for(var i = 0; i < x.length; i++){
-    
-    
-  }
+ 
 }
